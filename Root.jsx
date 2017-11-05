06e493db762b4f3fa8f79ec92d2e5094b96acc81
arn-the-long-beard <arn.menant@gmail.com>
2017-11-05 16:25:01 +0100

    Update Root.jsx

--- Root.jsx
+++ Root.jsx
@@ -2,20 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Route, Switch } from 'react-router-dom'
 import ContextProvider from '../ContextProvider'
-import AppBar from './features/Session/features/AppBar/index'
-import Footer from './features/BottomSide/Footer'
+
 import Home from './features/Tests/home'
-const Root = ({ store, Router, location, context }) => (
+const Root = ({ Router, location, context }) => (
 
 
     <Router location={location} context={context}>
       <ContextProvider context={context}>
         <div>
-          <AppBar />
           <Switch>
             <Route exact path='/' component={Home} />
           </Switch>
-          <Footer />
         </div>
       </ContextProvider>
     </Router>
